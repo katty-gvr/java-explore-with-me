9c966ea2c87def793909f8dd95fcb985bedde32b
Ekaterina <katya-3105@yandex.ru>
2023-06-30 23:33:16 +0300

    Исправлены замечания в StatClient и StatServiceImpl

--- stats-server/stat-dto/src/main/java/ru/practicum/dto/HitDto.java
+++ stats-server/stat-dto/src/main/java/ru/practicum/dto/HitDto.java
@@ -1,12 +1,10 @@
 package ru.practicum.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
